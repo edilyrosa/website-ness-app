f94e4b35df6ec5167db3ee830cf20b04677a666c
Edily Mora <edilyrosa@gmail.com>
2024-06-23 17:10:45 -0400

    correting error of type

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 import Hero from './components/Hero';
 import Products from './components/Products';
 import Contact from './components/Contact';
-import Image from 'next/image';
+
 
 export default function Page() {
   return (
@@ -18,14 +18,14 @@ export default function Page() {
             <img className="h-40 center-img" src="/NESS-logo.jpg" alt="Logo" />
             <p className="max-w-7xl mx-auto text-justify mt-4">
              🎀 Somos una pequeña empresa en el estado Zulia-Venezuela en donde amamos la moda y el estilo unico; nos caracterizamos con valores como
-              la responsabilidad, el amor, el respto, y ademas de ello siempre con actitud positiva y por supuesto con mucho estilo.
+              la responsabilidad, el amor, el respeto, y ademas de ello siempre con actitud positiva y por supuesto con mucho estilo.
               <br/>
               Mision: Diseñar y crear estilos de vestir unicos promoviendo el amor con cada una de las prendas. 
               <br/>
               Vision: Permanecer como una marca autentica, distintiva y con buen sentido de la moda.             
           </p>
           <p className="max-w-7xl mx-auto text-justify mt-4">
-            📍 Nuestro taller artesanal está ubicado en arrio 28 de Diciembre Av. 49 FI, el municipio San Francisco del estado Zulia. Allí es donde ocurre la magia, nos inspiramos en ti y tu estilo de vida. Estamos completamente dedicados a la confección para ofercer estilos vanguardistas.
+            📍 Nuestro taller artesanal está ubicado en barrio 28 de Diciembre Av. 49 FI, el municipio San Francisco del estado Zulia. Allí es donde ocurre la magia, nos inspiramos en ti y tu estilo de vida. Estamos completamente dedicados a la confección para ofrecer estilos vanguardistas.
           </p>
           <p className="max-w-7xl mx-auto text-justify mt-4">
             Te brindamos asesoría online y comercializamos nuestros prendas de forma digital, visita en nuestro Instagram <b><a href="https://www.instagram.com/ness7_3" target="_blank" rel="noopener noreferrer"><i>@ness7_3.</i></a></b>
